c588eec024833917112a9fd5e4f1d3e2c80286ca
Ethan Chen <intervigil@gmail.com>
2016-10-30 12:44:42 +0200

    bach: Add GraphicBuffer shim for camera
    * Needed for GraphicBuffer ABI change
    
    Change-Id: Idb1860a05b3669581951ac303bdefbb8ff0c8a99

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -67,3 +67,10 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MULTILIB := 32
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := camera.cpp
+LOCAL_SHARED_LIBRARIES := libui
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
