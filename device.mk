7d34b4a266b2eef602bba8024516b0a71a5a7374
TheScarastic <warabhishek@gmail.com>
2018-03-22 23:37:14 +0530

    bach: Import xiaomidoze from xiaomi/msm8996

--- device.mk
+++ device.mk
@@ -115,6 +115,10 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
