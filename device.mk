5a69bed473bc4d2a1066d9cc730c2a405ad63784
Soubhik_sk83 <soubhik.khan@gmail.com>
2018-08-09 17:30:51 +0530

    bach: Build XiaomiParts

--- device.mk
+++ device.mk
@@ -119,6 +119,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
