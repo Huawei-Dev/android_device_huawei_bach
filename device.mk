e6df5972f0745c2030fde280d257b87c07358017
Lakshman Chaluvaraju <lchalu@codeaurora.org>
2018-08-20 13:08:16 +0530

    bach: audio: add new vendor specific media codec file
    Add new vendor specific media codec file
    which contains vendor specific codec entries
    
    Change-Id: I818f3e3acbb1ec474c827c97b734746ff17c8201
    CRs-Fixed: 2289857

--- device.mk
+++ device.mk
@@ -222,7 +222,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/prebuilts/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(VENDOR_PATH)/prebuilts/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    $(VENDOR_PATH)/prebuilts/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
+    $(VENDOR_PATH)/prebuilts/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(VENDOR_PATH)/prebuilts/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
+    $(VENDOR_PATH)/prebuilts/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
