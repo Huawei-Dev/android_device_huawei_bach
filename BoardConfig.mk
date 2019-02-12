464e27568020c34520247fa2cc08a762b9361fc5
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-02-12 18:12:00 +0100

    bach : Remove deprecated cmdline option
    * Firmware path is specified in ueventd.qcom
    
    Change-Id: Ic5cd0eebaf4f4c18c70a8edd3ba2633290dfdceb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,7 +142,6 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_bach
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 slub_min_objects=12 androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000
 TARGET_KERNEL_ARCH := arm64
