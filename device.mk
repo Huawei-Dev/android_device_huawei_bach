3db0f2d6746dabdd246fd3ff8fa8e7cb3c0c45e0
Huawei-Dev <surdu_petru@yahoo.com>
2019-08-16 15:01:22 +0200

    bach: remove HIDL implementation

--- device.mk
+++ device.mk
@@ -153,12 +153,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common
