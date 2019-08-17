ae40cc31f1db9920bd75cafb7638a059abc39192
Grarak <williye97@gmail.com>
2019-08-17 11:25:35 +0200

    bach: Make sure proximity is a wake up sensor

--- device.mk
+++ device.mk
@@ -307,7 +307,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    sensors.nosensorhub.wrapper
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/prebuilts/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
