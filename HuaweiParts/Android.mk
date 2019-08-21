e10f116ebf3ac719430607e26c5c4273f8c7d499
Huawei-Dev <surdu_petru@yahoo.com>
2019-08-21 20:49:07 +0200

    bach: clean Android.mk on HuaweiParts

--- HuaweiParts/Android.mk
+++ HuaweiParts/Android.mk
@@ -18,14 +18,6 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_USE_AAPT2 := true
 
-package_resource_overlays := $(strip \
-    $(wildcard $(foreach dir, $(PRODUCT_PACKAGE_OVERLAYS), \
-      $(addprefix $(dir)/, packages/apps/XiaomiParts/res))) \
-    $(wildcard $(foreach dir, $(DEVICE_PACKAGE_OVERLAYS), \
-      $(addprefix $(dir)/, packages/apps/XiaomiParts/res))))
-
-LOCAL_RESOURCE_DIR := $(package_resource_overlays) $(LOCAL_RESOURCE_DIR)
-
 LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_DEX_PREOPT := false
 
