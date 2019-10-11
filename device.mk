24924115f95c9f7d23f4bce76f8c4ec063968348
Huawei-Dev <surdu_petru@yahoo.com>
2019-10-11 19:12:58 +0200

    bach: remove unused libFaceDetection

--- device.mk
+++ device.mk
@@ -93,9 +93,6 @@ PRODUCT_PACKAGES += \
     libshim_camera_hal \
     libshims_sensorlistener
 
-PRODUCT_PACKAGES += \
-    libFaceDetection
-
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/prebuilts/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
