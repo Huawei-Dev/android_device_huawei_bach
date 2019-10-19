3856706c93aa14558a2be3e2ffbeded4d1b2e601
Huawei-Dev <surdu_petru@yahoo.com>
2019-10-19 20:52:39 +0200

    bach: update power service to 1.2

--- device.mk
+++ device.mk
@@ -281,7 +281,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/prebuilts/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
