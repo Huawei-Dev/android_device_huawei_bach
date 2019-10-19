6b1471f29c2d677f6f463c35fff4094398f9616a
Huawei-Dev <surdu_petru@yahoo.com>
2019-10-19 15:45:46 +0200

    bach: add back hidl

--- device.mk
+++ device.mk
@@ -169,6 +169,12 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
+
 # HuaweiParts
 PRODUCT_PACKAGES += \
     HuaweiParts
