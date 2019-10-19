9fcbd372a78f9d46516bcf8dfb757f58019b02c9
Huawei-Dev <surdu_petru@yahoo.com>
2019-10-19 21:33:02 +0200

    bach: create etc/audio

--- device.mk
+++ device.mk
@@ -68,6 +68,7 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(VENDOR_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(VENDOR_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(VENDOR_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(VENDOR_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(VENDOR_PATH)/audio/audio_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(VENDOR_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
