aa5777d7ad64eb5c4a6d89621f64b369dd851022
Huawei-Dev <surdu_petru@yahoo.com>
2019-10-24 23:21:15 +0200

    bach: properly disable ril services & update to androidx

--- init/init_msm8937.cpp
+++ init/init_msm8937.cpp
@@ -41,16 +41,23 @@ void property_override_dual(char const system_prop[], char const vendor_prop[], 
     property_override(vendor_prop, value);
 }
 
+void property_override_3x(char const product_prop[], char const system_prop[], char const vendor_prop[], char const value[])
+{
+    property_override(product_prop, value);
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
 static void set_model(const char *model) {
     property_override("ro.hw.oemName", model);
     property_override("ro.build.product", model);
-    property_override_dual("ro.product.model", "ro.product.vendor.model", model);
+    property_override_3x("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", model);
 }
 
 void vendor_load_properties()
 {
     ifstream fin;
-    string buf,hwsim;
+    string buf;
 
     fin.open(APP_INFO, ios::in);
     if (!fin) {
@@ -64,33 +71,33 @@ void vendor_load_properties()
 
     fin.close();
 
-    hwsim = GetProperty("ro.boot.hwsim", "");
-    if (hwsim == "single") {
+    if ((buf.find("BAH-AL00") != string::npos) || (buf.find("BAH-L01") != string::npos)) {
+        android::init::property_set("persist.radio.noril", "1");
+    }
+
+    if ((buf.find("BAH-AL00") != string::npos) || (buf.find("BAH-L01") != string::npos) || (buf.find("BAH-L09") != string::npos) || (buf.find("CPN-AL00") != string::npos) || (buf.find("CPN-L0J") != string::npos) || (buf.find("CPN-L09") != string::npos)) {
 	property_override_dual("persist.multisim.config", "persist.radio.multisim.config", "ssss");
 	property_override("ro.telephony.default_network", "9");
 	property_override("ro.telephony.lteOnCdmaDevice", "0");
 	android::init::property_set("rild.libargs", "-d /dev/smd0");
-    } else {
-        android::init::property_set("persist.radio.noril", "1");
     }
 
     if ((buf.find("BAH-AL00") != string::npos) || (buf.find("BAH-L01") != string::npos) || (buf.find("BAH-L09") != string::npos) || (buf.find("BAH-W09") != string::npos)) {
 	property_override("ro.build.description", "BAH-L09-user 7.0 HUAWEIBAH-L09 C100B018 release-keys");
 	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "HUAWEI/BAH/HWBAH-Q:7.0/HUAWEIBAH-L09/C100B018:user/release-keys");
-	property_override_dual("ro.product.name", "ro.product.vendor.name", "BAH");
-	property_override_dual("ro.product.device", "ro.product.vendor.device", "HWBAH-Q");
-	property_override("ro.product.hardwareversion", "HL1TRTM");
+	property_override_3x("ro.product.name", "ro.product.system.name", "ro.product.vendor.name", "BAH");
+	property_override_3x("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "HWBAH-Q");
     }
 
     if ((buf.find("CPN-AL00") != string::npos) || (buf.find("CPN-L0J") != string::npos) || (buf.find("CPN-L09") != string::npos) || (buf.find("CPN-W09") != string::npos)) {
 	property_override("ro.build.description", "CPN-L09-user 7.0 HUAWEICPN-L09 C904B280 release-keys");
 	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "HUAWEI/CPN/HWCPN-Q:7.0/HUAWEICPN-L09/C904B280:user/release-keys");
-	property_override_dual("ro.product.name", "ro.product.vendor.name", "CPN");
-	property_override_dual("ro.product.device", "ro.product.vendor.device", "HWCPN-Q");
+	property_override_3x("ro.product.name", "ro.product.system.name", "ro.product.vendor.name", "CPN");
+	property_override_3x("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "HWCPN-Q");
     }
 
-    property_override_dual("ro.product.manufacturer", "ro.product.vendor.manufacturer", "HUAWEI");
-    property_override_dual("ro.product.brand", "ro.product.vendor.brand", "Huawei");
+    property_override_3x("ro.product.manufacturer", "ro.product.system.manufacturer", "ro.product.vendor.manufacturer", "HUAWEI");
+    property_override_3x("ro.product.brand", "ro.product.system.brand", "ro.product.vendor.brand", "Huawei");
     property_override_dual("ro.build.tags", "ro.vendor.build.tags", "release-keys");
     property_override("ro.hw.custPath", "/cust/hw/eu");
 
