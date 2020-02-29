41c99e9df69ee56d93741d8538d2c4e8afe53357
Pranav Vashi <neobuddy89@gmail.com>
2020-03-01 01:41:11 +0530

    bach: Update display config to version 1.9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -145,6 +145,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
+    
+PRODUCT_PACKAGES += \
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9_vendor
 
 # DRM
 PRODUCT_PACKAGES += \
