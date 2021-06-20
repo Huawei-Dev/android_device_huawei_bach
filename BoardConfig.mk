7a2f823eaa34c7813749e17d1fcfba2b1ea090bf
Huawei-Dev <surdu_petru@yahoo.com>
2021-06-20 11:03:27 +0200

    bach: update vendor security patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -238,7 +238,7 @@ TARGET_LD_SHIM_LIBS += \
 TARGET_USES_CUSTOM_THERMAL := true
 
 # Vendor Security patch level
-VENDOR_SECURITY_PATCH := 2020-12-05
+VENDOR_SECURITY_PATCH := 2021-06-05
 
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/soc/78db000.usb/msm_hsusb/gadget/lun%d/file
